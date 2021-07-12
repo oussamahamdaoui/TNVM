0758e6025c995c253f9886b9b04b6c962da278da
oussama hamdaoui <oussamahamdaoui@live.fr>
2021-07-12 13:38:25 +0200

    add sound

--- src/VM.js
+++ src/VM.js
@@ -1,7 +1,10 @@
-const {html, $} = require('@forgjs/noframework');
+const { html, $ } = require('@forgjs/noframework');
 
-const VM = ()=>{
-    const DomElement = html`<div class="vending-machine">
+const VM = (eventManager) => {
+  const DomElement = html`<div class="vending-machine">
+        <img class="graffiti" src="./graffiti.svg">
+        <img class="scratches" src="./scratches.svg">
+
         <div class="door">
             <div class="nail"></div>
             <div class="nail"></div>
@@ -102,49 +105,141 @@ const VM = ()=>{
         <div class="foot left"></div>
         <div class="foot right"></div>
         <div class="bottom"></div>
+        <div class="light"></div>
     </div>`;
 
+  const keyboard = $('.keyboard', DomElement);
+  const selectedItem = $('.selected-item', DomElement);
+  const trap = $('.trap', DomElement);
+  const motorSound = new Audio('./motor.mp3');
+  const bipSound = new Audio('/bip.mp3');
+  const bgSound = new Audio('./bg.mp3');
+  const flickerSound = new Audio('./flicker.mp3');
+  const dropSound = new Audio('./drop.mp3');
+  const openSound = new Audio('./open.mp3');
+  const audioFiles = [
+    {
+      name: 'motorSound',
+      audio: motorSound,
+      volume: 0.3,
+    }, {
+      name: 'bipSound',
+      audio: bipSound,
+      volume: 0.3,
+    }, {
+      name: 'bgSound',
+      audio: bgSound,
+      volume: 0.5,
+    }, {
+      name: 'flickerSound',
+      audio: flickerSound,
+      volume: 0.3,
+    }, {
+      name: 'dropSound',
+      audio: dropSound,
+      volume: 0.3,
+    }, {
+      name: 'openSound',
+      audio: openSound,
+      volume: 0.05,
+    },
+  ];
 
-    const keyboard = $('.keyboard', DomElement);
-    const selectedItem = $('.selected-item', DomElement);
-    const trap = $('.trap', DomElement);
+  audioFiles.forEach((f, i) => { audioFiles[i].audio.volume = 0; });
 
-    const drop = (elem) => {
-        if (!elem || !$('img', elem)) return;
-        $('img',elem).classList.add('drop');
-        if ($('img', trap).classList.contains('drop')) {
-            $('img', trap).className = '';
-        }
-        $('img', trap).src = $('img', elem).src;
-        elem.parentNode.classList.add('drop');
-        const css = window.getComputedStyle($('img', elem));
-        const timeout = parseInt(css.animationDuration) * 620;
-        const pos = Array.from($('img', elem).parentNode.parentNode.children).indexOf($('img', elem).parentNode);
-        
-        setTimeout(() => {
-            $('img', trap).classList.add(`pos${pos}`);
-            $('img', trap).classList.add('drop');
-        }, timeout);
-        
-        $('img', elem).addEventListener('animationend', () => {
-            $('img', elem).remove();
-        })
+  eventManager.subscribe('toggle-mute', () => {
+    if (audioFiles[0].audio.volume > 0) {
+      audioFiles.forEach((f, i) => { audioFiles[i].audio.volume = 0; });
+    } else {
+      audioFiles.forEach((f, i) => { audioFiles[i].audio.volume = f.volume; });
+      if (bgSound.paused) {
+        bgSound.play();
+      }
     }
+  });
 
-    keyboard.addEventListener('click', (e)=>{
-        if(!e.target.classList.contains('button')) return;
-        if(e.target.classList.contains('green')){
-            if(selectedItem.innerHTML.length < 2) return;
-            const id = selectedItem.innerHTML;
-            drop($(`[position="${id}"]`, DomElement));
-            selectedItem.innerHTML = "";
-        } else {
-            selectedItem.innerHTML = (selectedItem.innerHTML + e.target.innerHTML).slice(-2);
-        }
+  bgSound.addEventListener('timeupdate', () => {
+    const buffer = 0.4;
+    if (bgSound.currentTime > bgSound.duration - buffer) {
+      bgSound.currentTime = 0;
+      bgSound.play();
+    }
+  });
+
+  trap.addEventListener('mouseenter', () => {
+    openSound.play();
+  });
+
+  trap.addEventListener('mouseleave', () => {
+    openSound.play();
+  });
+
+  const drop = (elem) => {
+    if (!elem || !$('img', elem)) return;
+    $('img', elem).classList.add('drop');
+    if ($('img', trap).classList.contains('drop')) {
+      $('img', trap).className = '';
+    }
+    $('img', trap).src = $('img', elem).src;
+    elem.parentNode.classList.add('drop');
+    const css = window.getComputedStyle($('img', elem));
+    motorSound.play();
+    const timeout = parseInt(css.animationDuration, 10) * 620;
+    const pos = Array.from($('img', elem).parentNode.parentNode.children).indexOf($('img', elem).parentNode);
+
+    setTimeout(() => {
+      $('img', trap).classList.add(`pos${pos}`);
+      $('img', trap).classList.add('drop');
+      dropSound.play();
+    }, timeout);
+
+    $('img', elem).addEventListener('animationend', () => {
+      $('img', elem).remove();
     });
-    
+  };
 
-    return DomElement;
+  keyboard.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('button')) return;
+    bipSound.play();
+    if (e.target.classList.contains('green')) {
+      if (selectedItem.innerHTML.length < 2) return;
+      const id = selectedItem.innerHTML;
+      drop($(`[position="${id}"]`, DomElement));
+      selectedItem.innerHTML = '';
+    } else {
+      selectedItem.innerHTML = (selectedItem.innerHTML + e.target.innerHTML).slice(-2);
+    }
+  });
+
+  let flickerTime = Math.random() * 30000 + 500;
+  const light = $('.light', DomElement);
+
+  const flicker = () => {
+    if (light.classList.contains('flicker')) {
+      return;
+    }
+    light.classList.add('flicker');
+    flickerSound.play();
+    Array(10).fill().map((e, i) => {
+      setTimeout(() => {
+        if (light.classList.contains('flicker')) {
+          flickerSound.play();
+        }
+      }, i * 50);
+      return false;
+    });
+    flickerTime = Math.random() * 30000 + 500;
+    console.log(flickerTime);
+    setTimeout(flicker, flickerTime);
+  };
+
+  light.addEventListener('animationend', () => {
+    light.classList.remove('flicker');
+  });
+
+  setTimeout(flicker, flickerTime);
+
+  return DomElement;
 };
 
 module.exports = VM;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-const {html, $} = require('@forgjs/noframework');
+const { html, EventManager, $ } = require('@forgjs/noframework');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const feather = require('feather-icons');
 const VM = require('./VM');
 
 const App = () => {
-    const DomElement = html`
-    <div class="app">
-        ${VM()}
-        <div class="floor"></div>
-        <div class="light"></div>
-    </div>`;
-    
-    return DomElement;
-}
-document.body.append(App());
+  const eventManager = new EventManager();
 
+  const DomElement = html`
+    <div class="app">
+        <button class="button-sound mute">
+          ${feather.icons['volume-x'].toSvg()}
+          ${feather.icons['volume-1'].toSvg()}
+        </button>
+        ${VM(eventManager)}
+        <img class="graffiti2" src="graffiti2.svg">
+        <div class="floor"></div>
+    </div>`;
+  const toggleMuteButton = $('.button-sound', DomElement);
+
+  toggleMuteButton.addEventListener('click', () => {
+    eventManager.emit('toggle-mute');
+    toggleMuteButton.classList.toggle('mute');
+  });
+
+  return DomElement;
+};
+document.body.append(App());
